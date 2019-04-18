269e4c4ee8f754b7b66e05417c803bf1aa575cb3
Amber-Williams <ambercwilliams91@gmail.com>
2019-04-18 11:37:09 +0200

    Removed word press heavy oldUmi files

--- public_html/umi/oldUmi/_/wordpress/wp-content/themes/interior-lite/woocommerce.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="container">
-    		<div class="page_content">
-			<?php woocommerce_content(); ?>
-		   </div><!-- site-aligner -->
-    </div><!-- content -->
-     
-<?php get_footer(); ?>
