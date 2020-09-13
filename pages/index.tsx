83240b2bbb2133e0940d0eb86ae52f1a69434fd6
Amber-Williams <ambercwilliams91@gmail.com>
2020-09-13 18:04:19 +0100

    Prop type bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import CaseStudiesList from '../components/CaseStudiesList/CaseStudiesList'
 import { getAllPosts } from '../lib/api'
-import { CaseStudyList } from '../types/case-study'
+import { CaseStudyListProps } from '../types/case-study'
 
-const Home: React.FC<CaseStudyList> = ({ allCaseStudies }): JSX.Element => (
+const Home: React.FC<CaseStudyListProps> = ({
+  allCaseStudies,
+}): JSX.Element => (
   <React.Fragment>
     <Header />
     <main>
