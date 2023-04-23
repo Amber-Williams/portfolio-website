80b520feb40b1b53f3b091e666d0a170facc8e69
Amber Williams <amberwilliamsdev@gmail.com>
2023-04-23 17:40:16 +0100

    style: update study page style
    Also includes some hover styling and updates to the favicon

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,35 +9,40 @@ if (typeof window !== 'undefined') {
 
 const Header: React.FC = () => (
   <Head>
-    <title>Amber Williams Portfolio</title>
+    <title>Amber&apos;s portfolio</title>
+    <link
+      href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="images/favicon/apple-touch-icon.png"
+    />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="/images/favicon/favicon-32x32.png"
+      href="images/favicon/favicon-32x32.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="/images/favicon/favicon-16x16.png"
+      href="images/favicon/favicon-16x16.png"
     />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/images/favicon/apple-touch-icon.png"
-    />
-    <link rel="manifest" href="/images/favicon/site.webmanifest" />
+    <link rel="manifest" href="images/favicon/site.webmanifest" />
     <link
       rel="mask-icon"
-      href="/images/favicon/safari-pinned-tab.svg"
+      href="images/favicon/safari-pinned-tab.svg"
       color="#5bbad5"
     />
+    <meta name="msapplication-TileColor" content="#da532c" />
     <meta name="theme-color" content="#ffffff" />
-    <meta
-      name="google-site-verification"
-      content="e6S2XJHU76CL79dp9TwDvxOdd7DIGk995DxPBXvYp2c"
-    />
   </Head>
 )
 
