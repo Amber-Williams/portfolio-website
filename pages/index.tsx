45661b9a08a83f93b485c761b07ee3c909883f17
amber williams <amberwilliamsdev@gmail.com>
2025-08-23 00:54:12 +0100

    feat: Latest blogs section on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
+import { NextPage } from 'next'
 
 import About from '../components/About/About'
 import Blogs from '../components/BlogsSection'
 import Footer from '../components/Footer/Footer'
+import * as BlogLib from '../lib/blog'
+import { IBlogsListItem } from '../types'
 
-const Home = (): JSX.Element => (
+interface HomeProps {
+  blogs: IBlogsListItem[]
+}
+
+const Home: NextPage<HomeProps> = ({ blogs }) => (
   <React.Fragment>
     <About />
-    <Blogs />
+    <Blogs blogs={blogs} />
     <Footer />
   </React.Fragment>
 )
+
+export async function getServerSideProps() {
+  const blogs = await BlogLib.getAllBlogs(
+    process.env.CMS_SERVER,
+    process.env.CMS_API_KEY
+  )
+
+  return {
+    props: { blogs },
+  }
+}
 
 export default Home
