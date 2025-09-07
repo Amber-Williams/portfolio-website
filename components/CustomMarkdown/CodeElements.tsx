44d6725bdb2f65668fcee740f29e1d0ce1990aed
amber williams <amberwilliamsdev@gmail.com>
2025-09-07 23:47:11 +0100

    style: Custom markdown code blocks for text

--- components/CustomMarkdown/CodeElements.tsx
+++ components/CustomMarkdown/CodeElements.tsx
@@ -19,14 +19,37 @@ const SyntaxHighlightedCode = (props: any) => {
     }
   }, [props.className, props.children])
 
-  if (Boolean(props.className?.includes('lang')) === false) {
+  if (!Boolean(props.className?.includes('lang'))) {
     return <InlineCode>{props.children}</InlineCode>
+  }
+
+  if (Boolean(props.className?.includes('lang-txt'))) {
+    return (
+      <code
+        style={{
+          paddingTop: '1.5rem !important',
+          paddingBottom: '1.5rem !important',
+          paddingLeft: '1rem',
+          paddingRight: '1rem',
+          fontSize: breakpointSize === 'sm' ? '80%' : '100%',
+          color: 'var(--code-text-color)',
+          backgroundColor: 'var(--code-background-color)',
+        }}
+        {...props}
+        ref={ref}
+      />
+    )
   }
 
   return (
     <code
       style={{
+        paddingTop: '1.5rem !important',
+        paddingBottom: '1.5rem !important',
+        paddingLeft: '1rem',
+        paddingRight: '1rem',
         fontSize: breakpointSize === 'sm' ? '80%' : '100%',
+        backgroundColor: 'transparent',
       }}
       {...props}
       ref={ref}
@@ -40,12 +63,12 @@ const InlineCode = ({ children }: { children: string }) => {
   return (
     <code
       style={{
-        backgroundColor: '#f4f4f4',
+        backgroundColor: 'var(--code-background-color)',
         borderRadius: '4px',
         padding: '0.2rem 0.4rem',
         fontSize: breakpointSize === 'sm' ? '0.8rem' : '0.9rem',
         fontFamily: 'monospace',
-        color: 'var(--quad-color)',
+        color: 'var(--code-text-color)',
       }}
     >
       {children}
@@ -54,7 +77,11 @@ const InlineCode = ({ children }: { children: string }) => {
 }
 
 const PreCode = ({ children }: { children: any }) => {
+  const breakpointSize = Lib.useGetMediaQuerySize()
   const fencedLangs = [
+    'txt',
+    'md',
+    'markdown',
     'json',
     'yaml',
     'bash',
@@ -79,22 +106,39 @@ const PreCode = ({ children }: { children: any }) => {
     'go',
   ]
 
-  const lang = children.props.className.replace('lang-', '')
+  const lang = children.props?.className?.replace('lang-', '') || 'txt'
   const [copied, setCopied] = React.useState(false)
 
   const handleCopy = () => {
-    const code = children.props.children;
-    navigator.clipboard.writeText(code)
-      .then(() => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1000);
-      })
+    const code = children.props.children
+    navigator.clipboard.writeText(code).then(() => {
+      setCopied(true)
+      setTimeout(() => setCopied(false), 1000)
+    })
   }
 
   if (lang === 'mermaid') {
     return (
       <pre>
         <code className={'lang-mermaid'}>{children.props.children}</code>
+      </pre>
+    )
+  }
+
+  if (lang === 'txt') {
+    return (
+      <pre
+        style={{
+          padding: '0',
+          margin: '0',
+          fontSize: breakpointSize === 'sm' ? '0.8rem' : '0.9rem',
+          fontFamily: 'monospace',
+          color: 'var(--code-text-color)',
+          overflowX: 'auto',
+          whiteSpace: 'pre-wrap',
+        }}
+      >
+        {children}
       </pre>
     )
   }
@@ -143,7 +187,7 @@ const PreCode = ({ children }: { children: any }) => {
             padding: '0.2rem',
             borderRadius: '4px',
           }}
-          title={copied ? "Copied!" : "Copy to clipboard"}
+          title={copied ? 'Copied!' : 'Copy to clipboard'}
         >
           {copied ? (
             <svg
@@ -153,7 +197,7 @@ const PreCode = ({ children }: { children: any }) => {
               viewBox="0 0 24 24"
               style={{ fill: 'var(--success-color)' }}
             >
-              <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+              <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
             </svg>
           ) : (
             <img
@@ -169,11 +213,9 @@ const PreCode = ({ children }: { children: any }) => {
         style={{
           backgroundColor: '#011627',
           borderRadius: '4px',
-          padding: '0.2rem',
-          marginTop: '1.5rem',
-          marginBottom: '1.5rem',
           overflowX: 'auto',
           whiteSpace: 'pre-wrap',
+          maxHeight: '600px',
         }}
       >
         {children}
